4808efd2ed27739e1efdaa88532856543eb1726e
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2019-01-05 19:41:12 +0100

    Pretty good structure

--- kardashev.js
+++ kardashev.js
@@ -1,21 +1,23 @@
 
 var gameState = {
-  hunters : 0,
   energy: 0,
-  energy_per_hunt: 1,
-  energy_per_click : 2,
-  hunter_cost: 20,
-  hunter_cost_alpha: 1.1,
-  energy_per_farm: 1000,
-  farms : 0,
-  farm_cost: 20000,
-  farm_cost_alpha: 1.1,
   year : 1,
+  activities : {
+	hunt : {
+	  energy : 1
+	},
+	fish : {
+	  energy : 20
+	},
+	farming : {
+	  energy : 400
+	}
+  },
   research_tree : {
 	tool : {
 	  cost : 1000,
 	  onResearch : function() {
-		gameState.energy_per_hunt *= 1.25;
+		gameState.buildings['hunter'].power *= 1.25;
 		unlock_element("upgrade-tools");
 	  },
 	  prereq_of : ['fire', 'spear'],
@@ -24,7 +26,7 @@ var gameState = {
 	fire : {
 	  cost : 10000,
 	  onResearch : function() {
-		gameState.energy_per_hunt *= 1.25;
+		gameState.buildings['hunter'].power *= 1.25;
 	  },
 	  prereq_of : ['farm'],
 	  prereq : 0,
@@ -32,15 +34,33 @@ var gameState = {
 	spear : {
 	  cost : 15000,
 	  onResearch : function() {
-		gameState.energy_per_hunt *= 1.5;
+		gameState.buildings['hunter'].power *= 1.5;
 	  },
-	  prereq_of : ['bow'],
+	  prereq_of : ['bow', 'fishing'],
+	  prereq : 0,
+	},
+	fishing : {
+	  cost : 25000,
+	  onResearch : function () {
+		unlock_element("manual-fish");
+		unlock_building("fisher");
+		unlock_element("upgrade-harpoons");
+	  },
+	  prereq_of : ['boat'],
+	  prereq : 0,
+	},
+	boat : {
+	  cost : 40000,
+	  onResearch : function () {
+		gameState.buildings['fisher'].power *= 1.5;
+	  },
+	  prereq_of : ['rope'],
 	  prereq : 0,
 	},
 	bow : {
 	  cost : 30000,
 	  onResearch : function() {
-		gameState.energy_per_hunt *= 2.;
+		gameState.buildings['hunter'].power *= 2.;
 	  },
 	  prereq_of : ['farm'],
 	  prereq : 0,
@@ -48,25 +68,101 @@ var gameState = {
 	farm : {
 	  cost : 1000000,
 	  onResearch : function() {
-		unlock_element("buy-farm");
+		unlock_building("farm");
+		unlock_element("manual-farming");
+		unlock_element("upgrade-granary");
 	  },
-	  prereq_of : [],
+	  prereq_of : ['husbandry', 'plough'],
 	  prereq : 0,
+	},
+	plough : {
+	  cost : 1500000,
+	  onResearch : function() {
+		gameState.buildings['farm'].power *= 1.5;
+	  },
+	  prereq : 0,
+	  prereq_of : []
+	},
+	husbandry : {
+	  cost : 1300000,
+	  onResearch : function() {
+		gameState.buildings['farm'].power *= 2.5;
+	  },
+	  prereq : 0,
+	  prereq_of : ['yoke']
+	},
+	yoke : {
+	  cost : 2000000,
+	  onResearch : function() {
+		gameState.buildings['farm'].power *= 1.5;
+	  },
+	  prereq : 0,
+	  prereq_of : ['rope']
+	},
+	rope : {
+	  cost : 100000000,
+	  onResearch : function() {
+	  },
+	  prereq : 0,
+	  prereq_of : []
 	}
   },
   upgrades : {
 	tools : {
 	  cost : 500,
+	  level : 0,
 	  onUpgrade : function() {
-		gameState.energy_per_click *= 1.1;
+		gameState.activities['hunt'].energy *= 1.1;
 	  },
 	  alpha : 1.2
 	},
+	harpoons : {
+	  cost : 6000,
+	  level : 0,
+	  onUpgrade : function() {
+		gameState.activities['fish'].energy *= 1.15;
+	  },
+	  alpha : 1.2
+	},
+	granary : {
+	  cost : 60000,
+	  level : 0,
+	  onUpgrade : function() {
+		gameState.activities['farming'].energy *= 1.20;
+	  },
+	  alpha : 1.2
+	},
+  },
+  buildings : {
+	hunter : {
+	  total : 0,
+	  cost : 20,
+	  alpha : 1.05,
+	  power : 1
+	},
+	fisher : {
+	  total : 0,
+	  cost : 750,
+	  alpha : 1.05,
+	  power : 5
+	},
+	farm : {
+	  total : 0,
+	  cost : 15000,
+	  alpha : 1.1,
+	  power : 1000
+	}
   }
 };
 
 function unlock_element(elmname) {
 		document.getElementById(elmname).style.visibility = "initial";
+}
+
+function unlock_building(bname) {
+  unlock_element(bname+"-qty");
+  unlock_element("buy-"+bname);
+  unlock_element("stat-"+bname);
 }
 
 function initGame()
@@ -82,32 +178,34 @@ function initGame()
   for (resname in gameState.upgrades) {
 	document.getElementById("upgrade-"+resname).style.visibility = "hidden";
   }
+  for (resname in gameState.activities) {
+	document.getElementById("manual-"+resname).style.visibility = "hidden";
+  }
+
+  for (resname in gameState.buildings) {
+	document.getElementById("buy-"+resname).style.visibility = "hidden";
+	document.getElementById("stat-"+resname).style.visibility = "hidden";
+	document.getElementById(resname+"-qty").style.visibility = "hidden";
+  }
+  unlock_element("manual-hunt");
+  unlock_building("hunter");
   updateStats();
   var interval = setInterval(tick, 1000);
 }
 
-function manual_hunt()
+function activity(a)
 {
-  gameState.energy += gameState.energy_per_click;
+  gameState.energy += gameState.activities[a].energy;
   updateStats();
 }
 
-function buy_hunter()
+function buy(b)
 {
-  if (gameState.energy >= gameState.hunter_cost) {
-	gameState.energy -= gameState.hunter_cost;
-	gameState.hunters += 1;
-	gameState.hunter_cost *= gameState.hunter_cost_alpha;
-  }
-  updateStats();
-}
-
-function buy_farm()
-{
-  if (gameState.energy >= gameState.farm_cost) {
-	gameState.energy -= gameState.farm_cost;
-	gameState.farms += 1;
-	gameState.farm_cost *= gameState.farm_cost_alpha;
+  var building = gameState.buildings[b];
+  if (gameState.energy >= building.cost) {
+	gameState.energy -= building.cost;
+	building.total += 1;
+	building.cost *= building.alpha;
   }
   updateStats();
 }
@@ -134,24 +232,23 @@ function upgrade(resname)
 	gameState.energy -= res.cost;
 	res.onUpgrade();
 	res.cost *= res.alpha;
+	res.level += 1;
   }
 }
 
 function updateStats()
 {
   document.getElementById("stat-energy-qty").innerHTML = formatUnit(gameState.energy, "J");
-  document.getElementById("stat-hunters-qty").innerHTML = ""+gameState.hunters;
-  document.getElementById("stat-farms-qty").innerHTML = ""+gameState.farms;
   document.getElementById("stat-power-qty").innerHTML = formatUnit(totalPower(), "W");
-  document.getElementById("hunt-qty").innerHTML = formatUnit(gameState.energy_per_click, "J");
-  document.getElementById("hunter-cost").innerHTML = formatUnit(gameState.hunter_cost, "J");
-  document.getElementById("hunter-gain").innerHTML = formatUnit(gameState.energy_per_hunt, "W");
-  document.getElementById("farm-cost").innerHTML = formatUnit(gameState.farm_cost, "J");
-  document.getElementById("farm-gain").innerHTML = ""+formatUnit(gameState.energy_per_farm, "W");
   document.getElementById("stat-year").innerHTML = ""+gameState.year;
+  for (var bname in gameState.buildings) {
+	var b = gameState.buildings[bname];
+	document.getElementById(bname+"-qty").innerHTML = b.total;
+  }
   for (var upname in gameState.upgrades) {
 	var u = gameState.upgrades[upname];
 	document.getElementById("upgrade-"+upname+"-cost").innerHTML = formatUnit(u.cost, "J");
+	document.getElementById("upgrade-"+upname+"-level").innerHTML = u.level;
   }
   for (var resname in gameState.research_tree) {
 	var res = gameState.research_tree[resname];
@@ -168,12 +265,25 @@ function updateStats()
 	  }
 	}
   }
+  for (var resname in gameState.activities) {
+	var res = gameState.activities[resname];
+	document.getElementById(resname+"-qty").innerHTML = formatUnit(res.energy, "J");
+  }
+  for (var resname in gameState.buildings) {
+	var res = gameState.buildings[resname];
+	document.getElementById(resname+"-cost").innerHTML = formatUnit(res.cost, "J");
+	document.getElementById(resname+"-gain").innerHTML = formatUnit(res.power, "W");
+  }
 }
 
 function totalPower()
 {
-  return gameState.hunters * gameState.energy_per_hunt+
-	gameState.farms * gameState.energy_per_farm;
+  var s = 0;
+  for (var bname in gameState.buildings) {
+	var building = gameState.buildings[bname];
+	s += building.total * building.power;
+  }
+  return s;
 }
 
 function tick() {
