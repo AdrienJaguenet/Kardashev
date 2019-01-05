a37cb646ec62a61dc5b9f5d908bd0a716e6ce870
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2019-01-05 16:59:20 +0100

    Tech system overhaul

--- kardashev.js
+++ kardashev.js
@@ -10,8 +10,20 @@ var gameState = {
   farm_cost: 200000,
   farm_cost_alpha: 1.50,
   year : 1,
-  tech_farm_cost : 100000,
-  tech_farm_cost : 10000,
+  research_tree : {
+	tool : {
+	  cost : 10000,
+	  onResearch : function() {
+		gameState.energy_per_hunt *= 1.25;
+	  }
+	},
+	farm : {
+	  cost : 100000,
+	  onResearch : function() {
+		document.getElementById("buy-farm").style.visibility = "initial";
+	  }
+	}
+  }
 };
 
 function initGame()
@@ -47,41 +59,33 @@ function buy_farm()
   updateStats();
 }
 
-function research_farm()
+function research(resname)
 {
-  if (gameState.energy >= gameState.tech_farm_cost) {
-	document.getElementById("buy-farm").style.visibility = "initial";
-	document.getElementById("research-farm").style.visibility = "hidden";
-	gameState.energy -= gameState.tech_farm_cost;
+  res = gameState.research_tree[resname];
+  if (gameState.energy >= res.cost) {
+	document.getElementById("research-"+resname).style.visibility = "hidden";
+	gameState.energy -= res.cost;
+	res.onResearch();
   }
-  updateStats();
-}
-
-function research_tool()
-{
-  if (gameState.energy >= gameState.tech_tool_cost) {
-	document.getElementById("research-tool").style.visibility = "hidden";
-	gameState.energy -= gameState.tech_tool_cost;
-	gameState.energy_per_hunt *= 1.25;
-  }
-  updateStats();
 }
 
 
 function updateStats()
 {
-  document.getElementById("stat-energy-qty").innerHTML = ""+formatUnit(gameState.energy, "J");
+  document.getElementById("stat-energy-qty").innerHTML = formatUnit(gameState.energy, "J");
   document.getElementById("stat-hunters-qty").innerHTML = ""+gameState.hunters;
   document.getElementById("stat-farms-qty").innerHTML = ""+gameState.farms;
-  document.getElementById("stat-power-qty").innerHTML = ""+formatUnit(totalPower(), "W");
-  document.getElementById("hunt-qty").innerHTML = ""+formatUnit(gameState.energy_per_hunt, "J");
-  document.getElementById("hunter-cost").innerHTML = ""+formatUnit(gameState.hunter_cost, "J");
-  document.getElementById("hunter-gain").innerHTML = ""+formatUnit(gameState.energy_per_hunt, "W");
-  document.getElementById("farm-cost").innerHTML = ""+formatUnit(gameState.farm_cost, "J");
+  document.getElementById("stat-power-qty").innerHTML = formatUnit(totalPower(), "W");
+  document.getElementById("hunt-qty").innerHTML = formatUnit(gameState.energy_per_hunt, "J");
+  document.getElementById("hunter-cost").innerHTML = formatUnit(gameState.hunter_cost, "J");
+  document.getElementById("hunter-gain").innerHTML = formatUnit(gameState.energy_per_hunt, "W");
+  document.getElementById("farm-cost").innerHTML = formatUnit(gameState.farm_cost, "J");
   document.getElementById("farm-gain").innerHTML = ""+formatUnit(gameState.energy_per_farm, "W");
   document.getElementById("stat-year").innerHTML = ""+gameState.year;
-  document.getElementById("tech-farm-cost").innerHTML = ""+formatUnit(gameState.tech_farm_cost, "J");
-  document.getElementById("tech-tool-cost").innerHTML = ""+formatUnit(gameState.tech_tool_cost, "J");
+  for (var resname in gameState.research_tree) {
+	var res = gameState.research_tree[resname];
+	document.getElementById("tech-"+resname+"-cost").innerHTML = formatUnit(res.cost, "J");
+  }
 }
 
 function totalPower()
